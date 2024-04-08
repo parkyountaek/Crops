2d8f95b5e260d71cb18ab4cc273e9b3dbdaeff69
parkyountaek <yountaek11@gmail.com>
2024-04-08 23:15:48 +0900

    feat: init

---
+++ src/main/java/com/cropssystem/CropsSystemApplication.java
@@ -0,0 +1,13 @@
+package com.cropssystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CropsSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CropsSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cropssystem/CropsSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cropssystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CropsSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
